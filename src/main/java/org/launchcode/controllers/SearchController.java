427a1de758af17459b7133e1de483abac3faecc6
Aniya <aniyabtts09@gmail.com>
2019-06-20 16:05:58 -0500

    finished TODOs

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,13 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static com.sun.tools.doclint.Entity.or;
 
 /**
  * Created by LaunchCode
@@ -22,6 +25,23 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    // TODO #1 - Create handler to process search request and display resultsRe
+
+    @RequestMapping(value = "results")
+    public String results (Model model, @RequestParam String searchType, @RequestParam String searchTerm ) {
+
+        model.addAttribute("columns", ListController.columnChoices);
+        if (searchType.equals("all") || searchTerm.equals("")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
+        else{
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobs);
+
+        return "search";
+        }
+    }
 
 }
